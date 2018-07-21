e9a52813e330d646ddde93d59cc3e90baff2e1e3
Petteri Pulkkinen <petteri.pulkkinen@aalto.fi>
2018-07-21 10:11:16 +0300

    Add support for args parse
    Signed-off-by: Petteri Pulkkinen <petteri.pulkkinen@aalto.fi>

--- merge_xml_tool.py
+++ merge_xml_tool.py
@@ -1,8 +1,13 @@
 import xml.etree.ElementTree as ET
+import module_arguments
 import os
 
 place_holder = r'@PLACE_HOLDER'
-config = 'config.xml'
+config = r'config.xml'
+
+
+def _get_parent(element, tree):
+    return tree.find()
 
 
 def _make_tree(parent_file, child_file):
@@ -11,10 +16,10 @@ def _make_tree(parent_file, child_file):
     child_xml = os.path.abspath(child_file)
     child_tree = ET.parse(child_xml)
     for element in parent_tree.iter():
-        for key, value in element.items():
-            if value == place_holder:
-                f = child_tree.find('.//{}'.format(element.tag))
-                element.set(key, f.get(key))
+        generator = filter(lambda _, v: v == place_holder, element.items())
+        for k, _ in generator:
+            f = child_tree.find('.//{}'.format(element.tag))
+            element.set(k, f.get(k))
         if element.text == place_holder:
             f = child_tree.find('.//{}'.format(element.tag))
             element.text = f.text
@@ -29,6 +34,12 @@ def _get_config_files(directory):
 
 
 if __name__ == '__main__':
-    parent_file = 'parent.xml'
-    for child_file in _get_config_files(os.path.abspath('Jenkins')):
-        _make_tree(parent_file, child_file)
+    args = module_arguments.get_arguments()
+    parent_xml = os.path.abspath(args.parent)
+    if args.folder:
+        child_folder = os.path.abspath(args.child)
+        for child_xml in _get_config_files(child_folder):
+            _make_tree(parent_xml, child_xml)
+    else:
+        child_xml = os.path.abspath(args.child)
+        _make_tree(parent_xml, child_xml)
--- module_arguments.py
+++ module_arguments.py
@@ -7,5 +7,13 @@ def get_arguments():
     '''
     parser = argparse.ArgumentParser(
         description='Tool for merging xml files')
-    parser.add_argument(dest='parent', help='This is the help')
+    parser.add_argument('-p', '--parent',
+                        default='parent.xml',
+                        help='File used as parent in the merge process')
+    parser.add_argument('-c', '--child',
+                        default='child.xml',
+                        help='File used as child in the merge process')
+    parser.add_argument('-f', '--folder',
+                        action='store_true',
+                        help='Child object is handled as folder')
     return parser.parse_args()
